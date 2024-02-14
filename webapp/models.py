7cde59bb3e72fa5800f9685a05e68c5f43242396
JOE <mailforjoelabraham@gmail.com>
2024-02-14 17:38:24 +0530

    added a add record and update record

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    ph_no = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
